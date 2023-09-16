8efe3b55156f4eb768857e01bb54e65ac0de82c1
ssandrini <ssandrini@itba.edu.ar>
2023-09-16 12:25:35 -0300

    removing old code

--- gas-diffusion/src/main/java/ar/edu/itba/ss/models/Particle.java
+++ gas-diffusion/src/main/java/ar/edu/itba/ss/models/Particle.java
@@ -47,64 +47,6 @@ public class Particle {
         return id;
     }
 
-    public Set<Particle> getNeighbours() {
-        return neighbours;
-    }
-
-    public double getDistanceTo(Particle other) {
-        return Math.sqrt(
-                Math.pow(this.x - other.getX(), 2) +
-                Math.pow(this.y - other.getY(), 2)
-        ) - this.radius - other.getRadius();
-    }
-
-    public double getContourDistanceTo(Particle other, double dim) {
-        double dx = Math.abs(this.x - other.getX());
-        if(dx > dim / 2) {
-            dx = dim - dx;
-        }
-        double dy = Math.abs(this.y - other.getY());
-        if(dy > dim / 2) {
-            dy = dim - dy;
-        }
-        return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2)) - this.radius - other.getRadius();
-    }
-
-    public void move(double eta, int sideLength) {
-        double DEFAULT_SPEED = 0.03;
-        double accumulativeSin = Math.sin(this.direction);
-        double accumulativeCos = Math.cos(this.direction);
-        for(Particle neighbour : this.neighbours) {
-            accumulativeCos += Math.cos(neighbour.direction);
-            accumulativeSin += Math.sin(neighbour.direction);
-        }
-        double quantity = this.neighbours.size() + 1;
-        double sinAverage = accumulativeSin / quantity;
-        double cosAverage = accumulativeCos / quantity;
-        double noise = Math.random() * eta - eta / 2;
-        double nextDir = Math.atan2(sinAverage, cosAverage) + noise;
-//        this.setDirection((nextDir + 2 * Math.PI) % (2 * Math.PI));
-        this.setX((sideLength + this.x + this.vx) % sideLength);
-        this.setY((sideLength + this.y + this.vy) % sideLength);
-        this.setVx(DEFAULT_SPEED * Math.cos(this.direction));
-        this.setVy(DEFAULT_SPEED * Math.sin(this.direction));
-    }
-
-    public void addNeighbour(Particle neighbour) {
-        this.neighbours.add(neighbour);
-    }
-
-    public String neighboursToString() {
-        StringBuilder builder = new StringBuilder("[");
-        builder.append(id);
-        for(Particle neighbour : neighbours) {
-            builder.append(" ");
-            builder.append(neighbour.getId());
-        }
-        builder.append("]");
-        return builder.toString();
-    }
-
     public double getVx() {
         return vx;
     }
