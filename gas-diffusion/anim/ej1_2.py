ef1826860a39c71ff4b070e5d7c3c10edbfc848f
ssandrini <ssandrini@itba.edu.ar>
2023-09-17 17:10:51 -0300

    ej1_2

---
+++ gas-diffusion/anim/ej1_2.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def calculate_area(L):
+    return 0.09 * 0.09 + 0.09 * L
+
+L_values = [0.03, 0.05, 0.07, 0.09]
+
+average_pressures = {}
+std_dev_pressures = {}  # Agregado: diccionario para desvío estándar
+
+for L in L_values:
+    file_name = f"Pressure_280_L_{L}_v1.txt"
+    with open(file_name, 'r') as file:
+        lines = file.readlines()
+        pressures = [float(line.split()[1]) for line in lines]
+        average_pressure = np.mean(pressures)
+        std_dev_pressure = np.std(pressures)
+        average_pressures[L] = average_pressure
+        std_dev_pressures[L] = std_dev_pressure
+
+areas = [calculate_area(L) for L in L_values]
+average_pressures_list = [average_pressures[L] for L in L_values]
+std_dev_pressures_list = [std_dev_pressures[L] for L in L_values]
+
+inverse_areas = [1 / A for A in areas]
+
+plt.figure(figsize=(8, 6))
+plt.errorbar(inverse_areas, average_pressures_list, yerr=std_dev_pressures_list, fmt='x', color='green', capsize=5)  # Agregado: barras de error
+plt.xlabel("A⁻¹ (1/m²)", fontsize=15)
+plt.ylabel("Presión (kg/s$^2$)", fontsize=15)
+
+plt.grid(True)
+
+plt.show()
