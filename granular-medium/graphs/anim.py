f1be6df1dc8e4c9628974a495c3058ca56f765d5
Reversive <matiaspavan@gmail.com>
2023-10-26 02:15:42 -0300

    feat: particle animation

--- granular-medium/graphs/anim.py
+++ granular-medium/graphs/anim.py
@@ -26,5 +26,27 @@ plt.plot([(silo_width + silo_slit_width)/2, silo_width], [0, 0], 'k-', lw=2)
 plt.axis('off')
 plt.gca().set_aspect('equal', adjustable='box')
 
+# now animate the particles, each particle is a circle with an id, a position and a radius
+# you have for each time step, a list of particles that you need to animate
 
-plt.show()
+def animate(i):
+    plt.cla()
+    plt.plot([0, silo_width], [silo_height, silo_height], 'k-', lw=2)
+    plt.plot([0, 0], [0, silo_height], 'k-', lw=2)
+    plt.plot([silo_width, silo_width], [0, silo_height], 'k-', lw=2)
+    plt.plot([0, (silo_width - silo_slit_width)/2], [0, 0], 'k-', lw=2)
+    plt.plot([(silo_width + silo_slit_width)/2, silo_width], [0, 0], 'k-', lw=2)
+    plt.axis('off')
+    plt.gca().set_aspect('equal', adjustable='box')
+    for particle in particle_list[i]:
+        circle = plt.Circle((particle[1], particle[2]), particle[3], color='r')
+        plt.gca().add_patch(circle)
+    plt.title('Time: ' + str(i))
+    return circle, plt.title
+
+anim = animation.FuncAnimation(plt.gcf(), animate, frames=len(particle_list), interval=1, repeat=False)
+anim.save('animation.gif', writer='pillow', fps=60)
+
+
+
+#plt.show()
