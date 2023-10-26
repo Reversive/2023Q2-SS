64205db576c54e529303d8df8fb2f8b75b3b95ac
Reversive <matiaspavan@gmail.com>
2023-10-26 19:01:18 -0300

    feat: movable silo base

--- granular-medium/graphs/anim.py
+++ granular-medium/graphs/anim.py
@@ -16,26 +16,22 @@ with open('results.txt', 'r') as f:
 silo_width = 20
 silo_height = 70
 silo_slit_width = 3
-
-plt.plot([0, silo_width], [silo_height, silo_height], 'k-', lw=2)
-plt.plot([0, 0], [0, silo_height], 'k-', lw=2)
-plt.plot([silo_width, silo_width], [0, silo_height], 'k-', lw=2)
-plt.plot([0, (silo_width - silo_slit_width)/2], [0, 0], 'k-', lw=2)
-plt.plot([(silo_width + silo_slit_width)/2, silo_width], [0, 0], 'k-', lw=2)
-
-plt.axis('off')
-plt.gca().set_aspect('equal', adjustable='box')
+A = 0.15  # Amplitude in cm
+w = 5     # Angular frequency in rad/s
+t_step = 0.1  # Time step
 
 # now animate the particles, each particle is a circle with an id, a position and a radius
 # you have for each time step, a list of particles that you need to animate
 
 def animate(i):
     plt.cla()
+    t = i * t_step
+    y_bottom = A * math.sin(w * t)  # Calculate the y-position of the bottom side
     plt.plot([0, silo_width], [silo_height, silo_height], 'k-', lw=2)
     plt.plot([0, 0], [0, silo_height], 'k-', lw=2)
     plt.plot([silo_width, silo_width], [0, silo_height], 'k-', lw=2)
-    plt.plot([0, (silo_width - silo_slit_width)/2], [0, 0], 'k-', lw=2)
-    plt.plot([(silo_width + silo_slit_width)/2, silo_width], [0, 0], 'k-', lw=2)
+    plt.plot([0, (silo_width - silo_slit_width)/2], [y_bottom, y_bottom], 'k-', lw=2)
+    plt.plot([(silo_width + silo_slit_width)/2, silo_width], [y_bottom, y_bottom], 'k-', lw=2)
     plt.axis('off')
     plt.gca().set_aspect('equal', adjustable='box')
     for particle in particle_list[i]:
